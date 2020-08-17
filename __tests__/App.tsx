8f27109621aff25b40131b80a6a2fab4c19eebf1
Damon Ezard <damonezard@gmail.com>
2020-08-17 11:50:46 +0800

    Fix tests and convert to TypeScript

---
+++ __tests__/App.tsx
@@ -0,0 +1,11 @@
+import "react-native";
+import React from "react";
+
+const App = require("../App.tsx").default;
+
+// Note: test renderer must be required after react-native.
+import renderer from "react-test-renderer";
+
+it("renders correctly", () => {
+  renderer.create(<App />);
+});
