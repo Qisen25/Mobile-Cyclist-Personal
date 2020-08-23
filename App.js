8e26fbf4315283b55be20821c9dd91a588e22858
Deborah Ezard <damonezard@gmail.com>
2020-08-23 22:25:19 +0800

    Add location tracking component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import TrackingToggle from "./src/components/TrackerToggle";
+import * as TaskManager from "expo-task-manager";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing pull request</Text>
+
     </View>
   );
 }
